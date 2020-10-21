b011e42e546fd54a9c780ece962d8467ce40bc56
Thanh Bui <mrkieubui@gmail.com>
2020-10-21 17:27:35 +0700

    first commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = normalizePort(process.env.PORT || 5000)
 const app = express();
 const dev = app.get('env') !== 'production';
 
-if(dev){
+if(!dev){
     app.disable('x-powered-by');
     app.use(compression());
     app.use(morgan('common'));
@@ -22,7 +22,7 @@ if(dev){
     });
 }
 
-if(!dev){
+if(dev){
     app.use(morgan('dev'));
 }
 
